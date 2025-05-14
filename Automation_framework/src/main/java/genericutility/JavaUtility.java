8828aab317e69fb4e9381f8e20293a6c353a22c7
vphari17 <vphariprasad2@gmail.com>
2025-05-14 15:32:22 +0530

    Update JavaUtility.java

--- Automation_framework/src/main/java/genericutility/JavaUtility.java
+++ Automation_framework/src/main/java/genericutility/JavaUtility.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public class JavaUtility {
 	public String getSystemtTime() {
 		return LocalDateTime.now().toString().replace(":", "-");
+		return LocalDateTime.now().toString().replace(":", "-");
 		
 	}
 
