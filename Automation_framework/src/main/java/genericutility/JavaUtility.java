d34f4a22393b89083c96ed26eecc7018a400a236
Hari <Hari@DESKTOP-TIGA71I>
2025-05-14 16:00:27 +0530

    modification in java utility

--- Automation_framework/src/main/java/genericutility/JavaUtility.java
+++ Automation_framework/src/main/java/genericutility/JavaUtility.java
@@ -6,7 +6,7 @@ public class JavaUtility {
 	public String getSystemtTime() {
 		return LocalDateTime.now().toString().replace(":", "-");
 		return LocalDateTime.now().toString().replace(":", "-");
-		
+		return LocalDateTime.now().toString().replace(":", "-");
 	}
 
 }
